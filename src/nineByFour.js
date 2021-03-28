cf4cdc7696f1ada6f8a12659923d9005478de693
andre <andre_w3@yahoo.com>
2021-03-28 02:29:55 -0400

    working on event listener

--- src/nineByFour.js
+++ src/nineByFour.js
@@ -1,21 +1,30 @@
-const submitBtn = document.querySelector('submit');
-const rapperForm = document.getElementById(
-    'rapperForm', 
-    'artistName',
-    'stageName',
-    'home',
-    'recordLabel',
-    'numberOfAlbums',
-    'albumName',
-    'albumYear',
-    'diamondCert',
-    'multiPlatinumCert',
-    'platinumCert',
-    'goldCert',
-    'billboardHot',
-    'billboard200',
-    'billboardHotRAndB',
-    'topBillboard',
-    'submit'
-    );
-// const rapperForm = document.getElementById('rapperForm');
+window.addEventListener("load", function() {
+
+    const submitBtn = document.querySelector('submit');
+    const rapperForm = document.getElementById(
+        'rapperForm', 
+        'artistName',
+        'stageName',
+        'home',
+        'recordLabel',
+        'numberOfAlbums',
+        'albumName',
+        'albumYear',
+        'diamondCert',
+        'multiPlatinumCert',
+        'platinumCert',
+        'goldCert',
+        'billboardHot',
+        'billboard200',
+        'billboardHotRAndB',
+        'topBillboard',
+        'submit'
+        );
+        // const rapperForm = document.getElementById('rapperForm');
+        
+        submitBtn.addEventListener('submit', function(e) {
+            e.preventDefault();
+            console.log("hey");
+        });
+        
+    });
