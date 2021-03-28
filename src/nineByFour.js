b2b02fd600e13a165075ef57f337619e029627bc
andre <andre_w3@yahoo.com>
2021-03-28 01:48:01 -0400

    fixed form attributes and adding js

---
+++ src/nineByFour.js
@@ -0,0 +1,21 @@
+const submitBtn = document.querySelector('submit');
+const rapperForm = document.getElementById(
+    'rapperForm', 
+    'artistName',
+    'stageName',
+    'home',
+    'recordLabel',
+    'numberOfAlbums',
+    'albumName',
+    'albumYear',
+    'diamondCert',
+    'multiPlatinumCert',
+    'platinumCert',
+    'goldCert',
+    'billboardHot',
+    'billboard200',
+    'billboardHotRAndB',
+    'topBillboard',
+    'submit'
+    );
+// const rapperForm = document.getElementById('rapperForm');
