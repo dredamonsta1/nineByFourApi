bbe84c92bc422bb9203eca8dd872079a4404fb66
Andre Wilkinson <andre_w3@yahoo.com>
2024-11-16 02:44:32 -0500

    testing app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const sqlite3 = require('sqlite3');
 
-const db =  new sqlite3.Database("./qoute.db", sqlite3.OPEN_READWRITE, (err) => {
+const db =  new sqlite3.Database("./quote.db", sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err);
 });
 
