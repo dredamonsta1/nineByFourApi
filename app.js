e10a21ddae3d4d1297f78d0bacfd8d2ab0b69088
Andre Wilkinson <andre_w3@yahoo.com>
2024-11-18 20:44:37 -0500

    testing table.js

--- app.js
+++ app.js
@@ -6,9 +6,25 @@ const app = express();
 
 const sqlite3 = require('sqlite3');
 
-const db =  new sqlite3.Database("./quote.db", sqlite3.OPEN_READWRITE, (err) => {
+const db =  new sqlite3.Database("./quote.db", sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) return console.error(err);
 });
+
+// Define the SQL statement
+const sql = `
+CREATE TABLE IF NOT EXISTS quote (
+    ID INTEGER PRIMARY KEY,
+    name TEXT,
+    aka TEXT,
+    genre TEXT,
+    count INTEGER,
+    state TEXT,
+    region TEXT,
+    label TEXT,
+    album TEXT,
+    year INTEGER,
+    certifications TEXT
+)`;
 
 app.use(bodyParser.json());
 
@@ -23,7 +39,7 @@ app.post('/quote', (req, res) => {
     }
     catch (error) {
         return res.json({
-            statu: 400,
+            status: 400,
             success: false,
         })
     }
--- table.js
+++ table.js
@@ -7,3 +7,19 @@ const db =  new sqlite3.Database("./quote.db", sqlite3.OPEN_READWRITE | sqlite3.
 
 const sql = `CREATE TABLE quote(ID INTEGER PRIMARY KEY, name, aka, genre, count, state, region, label, album, year, certifications)`;
 db.run(sql);
+
+// Define the SQL statement
+// const sql = `
+// CREATE TABLE IF NOT EXISTS quote (
+//     ID INTEGER PRIMARY KEY,
+//     name TEXT,
+//     aka TEXT,
+//     genre TEXT,
+//     count INTEGER,
+//     state TEXT,
+//     region TEXT,
+//     label TEXT,
+//     album TEXT,
+//     year INTEGER,
+//     certifications TEXT
+// )`;
