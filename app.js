6559e7743054a43fe9b92c3ba97916853f296c4c
Andre Wilkinson <andre_w3@yahoo.com>
2024-11-21 01:59:51 -0500

    attempting to add data to db

--- app.js
+++ app.js
@@ -7,33 +7,39 @@ const app = express();
 
 
 const sqlite3 = require('sqlite3');
-
+let sql;
 const db =  new sqlite3.Database("./quote.db", sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) return console.error("this is line 12",err);
 });
 
 // Define the SQL statement
-const sql = `
-CREATE TABLE IF NOT EXISTS quote (
-    ID INTEGER PRIMARY KEY,
-    name TEXT,
-    aka TEXT,
-    genre TEXT,
-    count INTEGER,
-    state TEXT,
-    region TEXT,
-    label TEXT,
-    album TEXT,
-    year INTEGER,
-    certifications TEXT
-)`;
+// const sql = `
+// CREATE TABLE IF NOT EXISTS quote (
+//     ID INTEGER PRIMARY KEY,
+//     artistName TEXT,
+//     aka TEXT,
+//     genre TEXT,
+//     count INTEGER,
+//     state TEXT,
+//     region TEXT,
+//     label TEXT,
+//     album TEXT,
+//     year INTEGER,
+//     certifications TEXT
+// )`;
 
 app.use(bodyParser.json());
 
 // post request
-app.post('/artist', (req, res) => {
+app.post('/quote', (req, res) => {
     try {
-        console.log(req.body.name)
+        // console.log(req.body.aka)
+        const { artistName, aka, genre, count, state, region, label, album, year, certifications } = req.body;
+        sql = "INSERT INTO quote(artistName, aka, genre, count, state, region, label, album, year, certifications) VALUES (?,?,?,?,?,?,?,?,?,?)"
+        db.run(sql, [artistName, aka, genre, count, state, region, label, album, year, certifications], (err) => {
+            if (err) return res.json({ status: 300, success: false, error: err });
+            console.log('successful input ', artistName, aka, genre, count, state, region, label, album, year, certifications);
+        })
         res.json({
             status: 200,
             success: true,
