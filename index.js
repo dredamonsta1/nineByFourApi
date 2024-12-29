6541ad399dfb175e45892f91c387b8b25cc182b4
Andre Wilkinson <andre_w3@yahoo.com>
2024-12-28 20:52:49 -0500

    cors error

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import { DB } from "./connect.js";
 import express from 'express';
 import bodyParser from "body-parser";
 
+import cors from 'cors';
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/api', (req, res) => { 
