6b840cb2ef078ab277f8048f6e42aacc7c22da84
Andre Wilkinson <andre_w3@yahoo.com>
2025-01-30 15:59:34 -0500

    creating user DB

--- connect.js
+++ connect.js
@@ -1,16 +1,22 @@
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
 const sql3 = sqlite3.verbose();
 
 // const DB = new sqlite3.Database(':memory', sqlite3.OPEN_READWRITE, connected);
 // const DB = new sqlite3.Database('', sqlite3.OPEN_READWRITE, connected);
-const DB = new sql3.Database('./rapper.db', sqlite3.OPEN_READWRITE, connected);
+
+const DB = new sql3.Database("./rapper.db", sqlite3.OPEN_READWRITE, connected);
+const DB2 = new sql3.Database(
+  "./user.db",
+  sqlite3.OPEN_READWRITE || sqlite3.OPEN_CREATE,
+  connected
+);
 
 function connected(err) {
-    if (err) {
-        console.log(err.message);
-        return;
-    }
-    console.log('Created the DB or SQLite DB does already exist');
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+  console.log("Created the DB or SQLite DB does already exist");
 }
 
 let sql = `CREATE TABLE IF NOT EXISTS rappers(
@@ -27,13 +33,30 @@ artist_id INTEGER PRIMARY KEY,
      year INTEGER NOT NULL,
      certifications TEXT
 )`;
+
+let sql2 = `CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL,
+    role TEXT NOT NULL
+)`;
+
 DB.run(sql, [], (err) => {
-    //callback function
-    if (err) {
-        console.log(err,'error creating rappers table');
-        return;
-    }
-    console.log('CREATED TABLE');
-    
-})
-export { DB };
+  //callback function
+  if (err) {
+    console.log(err, "error creating rappers table");
+    return;
+  }
+  console.log("CREATED TABLE");
+});
+
+DB2.run(sql2, [], (err) => {
+  //callback function
+  if (err) {
+    console.log(err, "error creating users table");
+    return;
+  }
+  console.log("CREATED TABLE");
+});
+export { DB, DB2 };
--- index.js
+++ index.js
@@ -1,105 +1,154 @@
-import { DB } from "./connect.js";
+import { DB, DB2 } from "./connect.js";
+// import { DB2 } from "./connect.js";
 
-import express from 'express';
+import express from "express";
 import bodyParser from "body-parser";
 
-import cors from 'cors';
+import cors from "cors";
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/api', (req, res) => { 
-    //get all rappers from table
-    res.set('content-type', 'application/json');
-    const sql = 'SELECT * FROM rappers';
-    let data = { rappers: [] };
-    try {
-        DB.all(sql, [], (err, rows) => {
-            if (err) {
-                throw err; //let catch handle it
-            }
-            rows.forEach(row => {
-                data.rappers.push({
-                    artist_id: row.artist_id,
-                    name: row.artist_name,
-                    genre: row.genre,
-                    count: row.count,
-                    state: row.state,
-                    region: row.region,
-                    label: row.label,
-                    mixtape: row.mixtape,
-                    album: row.album,
-                    year: row.year,
-                    certifications: row.certifications
-                })
-            });
-            let content = JSON.stringify(data);
-            res.send(content);
-        })
-    } catch (err) {
-        console.log(err.message);
-        res.status(467);
-        res.send(`{ 'code':467, 'status':'${err.message}' }`);
-    }
-
+app.get("/api", (req, res) => {
+  //get all artists from table
+  res.set("content-type", "application/json");
+  const sql = "SELECT * FROM rappers";
+  let data = { rappers: [] };
+  try {
+    DB.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err; //let catch handle it
+      }
+      rows.forEach((row) => {
+        data.rappers.push({
+          artist_id: row.artist_id,
+          name: row.artist_name,
+          genre: row.genre,
+          count: row.count,
+          state: row.state,
+          region: row.region,
+          label: row.label,
+          mixtape: row.mixtape,
+          album: row.album,
+          year: row.year,
+          certifications: row.certifications,
+        });
+      });
+      let content = JSON.stringify(data);
+      res.send(content);
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(467);
+    res.send(`{ 'code':467, 'status':'${err.message}' }`);
+  }
 });
 
-app.get('/api', (req, res) => { 
-    //post request
-    res.set('content-type', 'application/json');
-    const sql = 'SELECT * FROM rappers';
+app.get("/api", (req, res) => {
+  //post request
+  res.set("content-type", "application/json");
+  const sql = "SELECT * FROM rappers";
 });
 
-app.post('/api', (req, res) => {
-    res.set('content-type', 'application/json');
-    const sql = 'INSERT INTO rappers(artist_name, aka, genre, count, state, region, label, mixtape, album, year, certifications) VALUES (?,?,?,?,?,?,?,?,?,?,?)';
-    let newArtistId;
+app.post("/api", (req, res) => {
+  res.set("content-type", "application/json");
+  const sql =
+    "INSERT INTO rappers(artist_name, aka, genre, count, state, region, label, mixtape, album, year, certifications) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+  let newArtistId;
 
-    try {
-        DB.run(sql, [req.body.artist_name, req.body.aka, req.body.genre, req.body.count, req.body.state, req.body.region, req.body.label, req.body.mixtape, req.body.album, req.body.year, req.body.certifications], function(err) {
-            if (err) throw err;
-            
-            newArtistId = this.lastID; //this refers to the last row inserted or provides the auto increment value
+  try {
+    DB.run(
+      sql,
+      [
+        req.body.artist_name,
+        req.body.aka,
+        req.body.genre,
+        req.body.count,
+        req.body.state,
+        req.body.region,
+        req.body.label,
+        req.body.mixtape,
+        req.body.album,
+        req.body.year,
+        req.body.certifications,
+      ],
+      function (err) {
+        if (err) throw err;
 
-            res.status(201);
-            let data = {status: 201, message: `new artist ${newArtistId} saved.`};
-            let content = JSON.stringify(data);
-            res.send(content);
+        newArtistId = this.lastID; //this refers to the last row inserted or provides the auto increment value
+
+        res.status(201);
+        let data = { status: 201, message: `new artist ${newArtistId} saved.` };
+        let content = JSON.stringify(data);
+        res.send(content);
+      }
+    );
+  } catch (err) {
+    console.log(err.message);
+    res.status(468);
+    res.send(`{ 'code':468, 'status':'${err.message}' }`);
+  }
+});
+
+app.delete("/api", (req, res) => {
+  res.set("content-type", "application/json");
+  const sql = "DELETE FROM rappers WHERE artist_id = ?";
+  try {
+    DB.run(sql, [req.query.artist_id], function (err) {
+      if (err) throw err;
+      if (this.changes === 1) {
+        //one item deleted
+        res.status(204);
+        res.send(
+          `{ 'code':204, 'message':'rapper ${req.query.artist_id}was deleted'}`
+        );
+      } else {
+        //no item deleted
+        res.status(204);
+        res.send(`{ 'code':204, 'message':'no operation done' }`);
+      }
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(469);
+    res.send(`{ 'code':469, 'status':'${err.message}' }`);
+  }
+});
+
+//users api
+app.get("/api/users", (req, res) => {
+  //get all users
+  res.set("content-type", "application/json");
+  const sql = "SELECT * FROM users";
+  let data = { users: [] };
+  try {
+    DB2.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err; //let catch handle it
+      }
+      rows.forEach((row) => {
+        data.users.push({
+          user_id: row.user_id,
+          username: row.username,
+          password: row.password,
+          email: row.email,
+          role: row.role,
         });
-    } catch (err) {
-        console.log(err.message);
-        res.status(468);
-        res.send(`{ 'code':468, 'status':'${err.message}' }`);
-    }
- });
-
-app.delete('/api', (req, res) => {
-    res.set('content-type', 'application/json');
-    const sql = 'DELETE FROM rappers WHERE artist_id = ?';
-    try {
-        DB.run(sql, [req.query.artist_id], function(err) {
-            if (err) throw err;
-            if (this.changes === 1) {
-                //one item deleted
-                res.status(204);
-                res.send(`{ 'code':204, 'message':'rapper ${req.query.artist_id}was deleted'}`);
-            } else {
-                //no item deleted
-                res.status(204);
-                res.send(`{ 'code':204, 'message':'no operation done' }`);
-            } 
-        });
-    } catch (err) {
-        console.log(err.message);
-        res.status(469);
-        res.send(`{ 'code':469, 'status':'${err.message}' }`);
-    }
- });
+      });
+      let content = JSON.stringify(data);
+      res.send(content);
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(467);
+    res.send(`{ 'code':467, 'status':'${err.message}' }`);
+  }
+});
 
 app.listen(process.env.PORT || 3010, (err) => {
-    if (err) {
-        console.log('ERROR:', err.message)
-    }
-    console.log('LISTENING on port 3010');
+  if (err) {
+    console.log("ERROR:", err.message);
+  }
+  console.log("LISTENING on port 3010");
 });
